1e332d3943541557573987fe56649f4077edd51c
Valentina Montaño <>
2024-11-24 17:18:39 -0600

    email

--- send_email.py
+++ send_email.py
@@ -8,11 +8,9 @@ port = int(os.environ.get('SMTP_PORT', 465))
 username = os.environ.get('USER_EMAIL')
 password = os.environ.get('USER_PASSWORD')
 
-# Obtener información de la rama desde las variables de entorno
+# Contenido del correo
 branch_name = os.environ.get('BRANCH_NAME', 'Unknown Branch')
 repository = os.environ.get('REPOSITORY_NAME', 'Unknown Repository')
-
-# Contenido del correo
 subject = f"Changes pushed to branch: {branch_name}"
 body = f"""
 Hola,
@@ -25,12 +23,15 @@ GitHub Actions
 """
 message = f"Subject: {subject}\n\n{body}"
 
-# Configuración y envío del correo
+# Conexión y envío
 context = ssl.create_default_context()
 try:
+    print(f"Connecting to SMTP server {smtp_server}:{port}")
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, username, message)
         print("Correo enviado con éxito")
-except Exception as e:
+except smtplib.SMTPException as e:
     print(f"Error al enviar el correo: {e}")
+except Exception as e:
+    print(f"Error inesperado: {e}")
